3c28879581ef41f4f3e0528546eafb2cac02eaf3
Pranav Paralkar <163521252+PranavParalkar@users.noreply.github.com>
2025-06-10 12:07:52 +0530

    Initial Project

---
+++ SpringBootProject/src/test/java/com/RestAPI/DemoProject/DemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestAPI.DemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
