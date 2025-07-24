2629e3a3c5c189c61872b1bf9c2f1f9d056ca4e1
PranavParalkar <pranavparalkar21@gmail.com>
2025-07-24 23:31:32 +0530

    Update order handling and UI for payment integration
    Refactored order creation to store amount in paise and updated Razorpay integration with test key in orders.html. Improved WalletModal UI with new button and color, changed redirect URL, and added allowed origin in CORS config. Also updated database password in application.properties.

--- ArtGalleryProject/src/main/java/com/RESTAPI/ArtGalleryProject/config/SecurityConfig.java
+++ ArtGalleryProject/src/main/java/com/RESTAPI/ArtGalleryProject/config/SecurityConfig.java
@@ -43,8 +43,8 @@ public class SecurityConfig {
 								"/auctions/bid/**",
 								"/user/profile",
 								"/licenses/**",
-								"/createOrder",
-								"/paymentCallback",
+//								"/createOrder",
+//								"/paymentCallback",
 								"/upload-painting")
 						.authenticated()
 						.anyRequest().permitAll())
@@ -57,7 +57,11 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+		config.setAllowedOrigins(List.of(
+			    "http://localhost:5173",
+			    "http://localhost:5174",
+			    "http://127.0.0.1:5500"
+			));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setAllowCredentials(true);
