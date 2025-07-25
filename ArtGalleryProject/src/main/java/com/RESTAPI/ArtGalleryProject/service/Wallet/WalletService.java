dd180e36e2fef7fabaec767760acbff3ebf4589a
PranavParalkar <pranavparalkar21@gmail.com>
2025-07-25 15:42:51 +0530

    remove wallet to reintegrate it

--- ArtGalleryProject/src/main/java/com/RESTAPI/ArtGalleryProject/service/Wallet/WalletService.java
+++
@@ -1,7 +0,0 @@
-package com.RESTAPI.ArtGalleryProject.service.Wallet;
-
-public interface WalletService {
-	public Object getBalance(long userId);
-
-	public Object updateBalance(long userId, double amount);
-}
