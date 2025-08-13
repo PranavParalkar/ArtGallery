e9a19b87cb1b240475a11d26cd07a4422f95eb1e
Varad <varadkolhe36@gmail.com>
2025-08-13 14:28:17 +0530

    transaction service

---
+++ ArtGalleryProject/src/main/java/com/RESTAPI/ArtGalleryProject/service/OrderService/TransactionServiceImpl.java
@@ -0,0 +1,44 @@
+package com.RESTAPI.ArtGalleryProject.service.OrderService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.RESTAPI.ArtGalleryProject.Entity.Painting;
+import com.RESTAPI.ArtGalleryProject.Entity.Transactions;
+import com.RESTAPI.ArtGalleryProject.Entity.User;
+import com.RESTAPI.ArtGalleryProject.Enum.TransactionType;
+import com.RESTAPI.ArtGalleryProject.repository.TransactionRepo;
+
+@Service
+public class TransactionServiceImpl implements TransactionService{
+
+	private static final Logger logger = LoggerFactory.getLogger(TransactionServiceImpl.class);
+	@Autowired
+	private TransactionRepo transactionRepo;
+	
+	@Override
+	public void createTransaction(User user, TransactionType type, double amount, Painting painting) {
+		Transactions transaction = new Transactions();
+		logger.info("New transaction created for user {}", user.getName());
+		transaction.setUser(user);
+		transaction.setType(type);
+		transaction.setAmount(amount);
+		transaction.setPainting(painting);
+		transactionRepo.save(transaction);
+		logger.info("Transaction saved for user {}", user.getName());
+	}
+
+	@Override
+	public void createTransaction(User user, TransactionType type, double amount) {
+		Transactions transaction = new Transactions();
+		logger.info("New transaction created for user {}", user.getName());
+		transaction.setUser(user);
+		transaction.setType(type);
+		transaction.setAmount(amount);
+		transactionRepo.save(transaction);
+		logger.info("Transaction saved for user {}", user.getName());
+	}
+	
+}
