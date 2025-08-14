e2bc5c6d300520a1f2806ba2a7bccc346e9b4b37
Varad <varadkolhe36@gmail.com>
2025-08-15 02:24:36 +0530

    transaction backend final commit

--- ArtGalleryProject/src/main/java/com/RESTAPI/ArtGalleryProject/service/OrderService/OrderService.java
+++ ArtGalleryProject/src/main/java/com/RESTAPI/ArtGalleryProject/service/OrderService/OrderService.java
@@ -9,7 +9,7 @@ import com.RESTAPI.ArtGalleryProject.Entity.Orders;
 import com.razorpay.RazorpayException;
 
 public interface OrderService {
-	public Orders updateStatusPayment(Map<String, String> map, long userId);
+	public Orders updateStatusPayment(Map<String, String> map);
 	public Orders createOrder(OrderRequest request) throws RazorpayException;
 	public String updateStatus(String email, long userId, double amount, long paintingId,
 			String mobileNumber, String Address, String paymentMethod, String name) 
